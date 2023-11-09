b57cc84b860f4696a52eb5fd683029de9a15a7fe
Serhii1727 <kravchenko087@ukr.net>
2023-11-09 13:36:03 -0500

    fix makeSchedule

--- src/js/make-schedue/make-schedule-first.js
+++ src/js/make-schedue/make-schedule-first.js
@@ -10,25 +10,48 @@ export function makeSceduleFirstTwoHours(employees) {
         .map(({ name }) => { return name });
     
     refs.trLhFsbOkies.querySelector('[data-first]').textContent = arrayRandomEmloyees[0];
+    refs.trLhFsbOkies.querySelector('[data-first]').classList.add(arrayRandomEmloyees[0])
+
     refs.trLhFsbRollOverSteam.querySelector('[data-first]').textContent = arrayRandomEmloyees[1];
+    refs.trLhFsbRollOverSteam.querySelector('[data-first]').classList.add(arrayRandomEmloyees[1]);
+
     refs.trRhFsbOkies.querySelector('[data-first]').textContent = arrayRandomEmloyees[2];
+    refs.trRhFsbOkies.querySelector('[data-first]').classList.add(arrayRandomEmloyees[2])
+
     refs.trRhFsbRollOverSteam.querySelector('[data-first]').textContent = arrayRandomEmloyees[3];
+    refs.trRhFsbRollOverSteam.querySelector('[data-first]').classList.add(arrayRandomEmloyees[3]);
+
     refs.trLhFscOkies.querySelector('[data-first]').textContent = arrayRandomEmloyees[4];
+    refs.trLhFscOkies.querySelector('[data-first]').classList.add(arrayRandomEmloyees[4]);
+
     refs.trLhFscRollOverSteam.querySelector('[data-first]').textContent = arrayRandomEmloyees[5];
+    refs.trLhFscRollOverSteam.querySelector('[data-first]').classList.add(arrayRandomEmloyees[5]);
+
     refs.trRhFscOkies.querySelector('[data-first]').textContent = arrayRandomEmloyees[6];
+    refs.trRhFscOkies.querySelector('[data-first]').classList.add(arrayRandomEmloyees[6]);
+
     refs.trRhFscRollOverSteam.querySelector('[data-first]').textContent = arrayRandomEmloyees[7];
+    refs.trRhFscRollOverSteam.querySelector('[data-first]').classList.add(arrayRandomEmloyees[7]);
+
     refs.trLhCushionInstall.querySelector('[data-first]').textContent = arrayRandomEmloyees[8];
+    refs.trLhCushionInstall.querySelector('[data-first]').classList.add(arrayRandomEmloyees[8])
+
     refs.trRhCushionInstall.querySelector('[data-first]').textContent = arrayRandomEmloyees[9];
+    refs.trRhCushionInstall.querySelector('[data-first]').classList.add(arrayRandomEmloyees[9])
     
     for (let i = 0; i < arrayRandomEmloyees.length; i += 1) {
+
         employees.find((employee) => {
             const { name } = employee;
+            
             if (name === arrayRandomEmloyees[i]) {
-                refs.trLhFsbOkies.querySelector('[data-first]').classList.add(name)
+                
         
                 const placeElement = document.querySelector(`.${name}`);
+                const parentElement = placeElement.parentNode;
+                console.log(parentElement)
                 
-                employee.lhFsbOkies.push(`${getDayOfWeek()} 6-30/8-30`)    
+                employee.lhFsbOkies.push(`${getDayOfWeek()} 6-30/8-30`)  
             }
         })
     }
